459b3abc3eaeec6e7f7949f474ca9b11d9284f83
Benson Michael Owen <lordkz19@gmail.com>
2024-04-24 02:26:36 +0100

    Submission Portal Corrected

--- journal/js/forms/createArticle.js
+++ journal/js/forms/createArticle.js
@@ -1,11 +1,17 @@
 import { EndPoint } from "../constants.js";
 
 const uploadForm = document.getElementById("uploadArticle");
+const inputFields = document.querySelectorAll("input")
+const submitButton = document.getElementById("submitButton")
 
 uploadForm.addEventListener("submit", function(e) {
     e.preventDefault();
     const formData = new FormData(uploadForm);
-
+    // inputFields.forEach(inputField => {
+    //     if(inputField.value && inputField.value !== "" && inputField.vallue !== " "){
+    //         inputField.setAttribute("readonly", "true")
+    //     }
+    // });
     fetch(`${EndPoint}/temporaryManuscriptUpload.php`, {
         method: 'POST',
         body: formData
@@ -15,12 +21,17 @@ uploadForm.addEventListener("submit", function(e) {
         console.log(data); // Log server response
         if(data.status === "success"){
             alert("Upload Successful")
-            window.location.href = "./issues.html"
+            window.location.href = "../issues.html"
         }else if(data.status === "error"){
             alert(data.message)
         }else{
             alert("Internal Server Error")
         }
+        submitButton.removeAttribute("disabled")
+        inputFields.forEach(field =>{
+            field.removeAttribute("readonly")
+        })
+
     })
     .catch(error => {
         console.error('Error:', error);
