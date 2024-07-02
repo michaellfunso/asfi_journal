a28fc336b22c09e9d77a06c10fd85134d562ad21
Benson Michael Owen <lordkz19@gmail.com>
2024-07-02 18:21:14 +0100

    Editors default Images updated

--- backend/addEditor.php
+++ backend/addEditor.php
@@ -9,16 +9,14 @@ $field = $_POST["field"];
 $country = $_POST["country"];
 $profileimage = $_FILES["photo"];
 
-// Get the filename and append it to the target directory
-$File = basename($_FILES["photo"]["name"]);
-$targetFile = $targetDir . $File;
-$newFileName = time() . '_' . $File;
+
 
 
 $quillContent = json_decode($_POST["bio"], true);
 
 $bio = json_encode($quillContent);
 $verifyCode = $_POST["verifyCode"];
+$newFileName = "";
 
 
 if(isset($_POST["prefix"])){
@@ -32,11 +30,20 @@ if(isset($_POST["prefix"])){
         $count = mysqli_num_rows($resultCode);
 
         if($count > 0){
- 
-            if (move_uploaded_file($_FILES["photo"]["tmp_name"], $targetFile)) {
-                // File uploaded successfully, now you can do something with the data
-                rename($targetDir . $_FILES["photo"]["name"], $targetDir.$newFileName);
+
+            if(isset($_FILES["photo"])){
+                // Get the filename and append it to the target directory
+                $File = basename($_FILES["photo"]["name"]);
+                $targetFile = $targetDir . $File;
+                $newFileName = time() . '_' . $File;
+                if (move_uploaded_file($_FILES["photo"]["tmp_name"], $targetFile)) {
+                    // File uploaded successfully, now you can do something with the data
+                    rename($targetDir . $_FILES["photo"]["name"], $targetDir.$newFileName);
+                }
+            }else{
+                $newFileName = "avatar.png";
             }
+      
 
     $stmt = $con->prepare("INSERT INTO `editors_list` (`prefix`, `fullname`, `bio`, `discipline`, `field`, `country`, `photo`) VALUES(?,?,?,?,?,?,?)");
     if(!$stmt){
